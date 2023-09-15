36ddd7734e066bd96af442a4c064e2d7c1e5817f
CraciDenisStefan <crdenis13@gmail.com>
2023-09-15 14:13:43 +0200

    Modificato index , creato card , ok

--- Back-End/resources/js/app.js
+++ Back-End/resources/js/app.js
@@ -4,3 +4,22 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+// Ottieni l'elemento HTML con id 'description'
+let descriptElement = document.getElementById('description');
+
+// Estrai il testo dall'elemento
+let descript = descriptElement.textContent;
+
+function truncateText(descript) {
+    if (descript.length > 50) {
+        return descript.substr(0, 50) + '...';
+    }
+    return descript;
+}
+
+// Utilizza la funzione per abbreviare il testo
+let truncatedDescription = truncateText(descript);
+
+// Assegna il testo abbreviato all'elemento HTML
+descriptElement.textContent = truncatedDescription;
--- Back-End/resources/views/admin/apartments/index.blade.php
+++ Back-End/resources/views/admin/apartments/index.blade.php
@@ -2,58 +2,65 @@
 
 @section('content')
 
-<div class="container">
-<div class="col-12 col-sm-6 col-md-3 my-5 w-100">
-  <div class="d-flex justify-content-between align-items-center">
-    <div>
-      <a href="{{route('admin.apartments.create')}}" class="btn btn-primary">Aggiungi un appartamento</a>
+<div class="background_index">
+  <div class="container">
+    <div class="col-12 col-sm-6 col-md-3 py-5 w-100">
+      <div class="d-flex justify-content-between align-items-center">
+        <div>
+          <a href="{{route('admin.apartments.create')}}" class="btn btn-primary">Aggiungi un appartamento</a>
+        </div>
+        <div>
+          <a href="{{route('admin.dashboard')}}" class="btn btn-primary">Torna alla dashboard</a>
+        </div>
+      </div>
     </div>
+  </div>
+      
+  <div class="container">
     <div>
-      <a href="{{route('admin.dashboard')}}" class="btn btn-primary">Torna alla dashboard</a>
+      <h3>I tuoi appartamenti</h3>
+    </div>
+    <div class="row ">
+      @foreach($apartments as $apartment)
+        <div class="col-12 col-lg-6 mb-4"> 
+          <div class="card border m-2 d-flex align-items-stretch" >
+            <div class="row g-0">
+              <div class="col-md-4">
+                @if($apartment->img)
+                <div class="card-img my_card"
+                  style="background-image:url('{{ asset('storage/'.$apartment->img) }}'); background-size: cover; background-position: center; height: 100%;">
+                </div>
+                @else
+                <div class="card-img my_card"
+                style="background-image: url('https://cdn.garneroarredamenti.com/media/catalog/product/cache/1/image/1200x/040ec09b1e35df139433887a97daa66f/1/0/10241_166253_cucina_completa_moderna_shabby_componibile_240cm_bianca_rovere_oxford-9.1686601425.jpg'); background-size: cover; background-position: center; height: 100%;">
+              </div>
+                @endif
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">{{$apartment->title}}</h5>
+                  <p class="card-text" id="description">{{$apartment->description}}</p>
+                  <p class="card-text">
+                    <div class="d-flex ">
+                      <a class="btn btn-sm btn-primary" href="{{route('admin.apartments.show', $apartment->id)}}"><i class="fa-solid fa-eye"></i></a>
+                      <a class="btn btn-sm btn-warning mx-2" href="{{route('admin.apartments.edit', $apartment->id)}}"><i class="fa-solid fa-pen-to-square"></i></a>
+                      <form class="form-delete" action="{{route('admin.apartments.destroy', $apartment->id)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-sm btn-danger">
+                          <i class="fa-solid fa-trash-can"></i>
+                        </button>
+                      </form>
+                    </div>
+                 </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      @endforeach
     </div>
   </div>
 </div>
-
-
-    <div class="card mb-3">
-      <div class="card-header">
-        <h3>I nostri B&B</h3>
-      </div>
-      <div class="card-body">
-        <table class="table table-striped">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Slug</th>
-                <th scope="col">Prezzo</th>
-              </tr>
-            </thead>
-            <tbody>
-                @foreach($apartments as $apartment)
-              <tr>
-                <th scope="row">{{$apartment->id}}</th>
-                <td>{{$apartment->title}}</td>
-                <td>{{$apartment->slug}}</td>
-                <td>{{$apartment->price}}</td>
-                <td>
-                  <div class="d-flex align-items-center justify-content-between my-content">
-                      <a class="btn btn-sm btn-primary" href="{{route('admin.apartments.show', $apartment->id)}}"><i class="fa-solid fa-eye"></i></a>
-                      <a class="btn btn-sm btn-warning" href="{{route('admin.apartments.edit', $apartment->id)}}" class="mx-3"><i class="fa-solid fa-pen-to-square"></i></a>
-                      <form class="form-delete" action="{{route('admin.apartments.destroy', $apartment->id)}}" method="POST">
-                          @csrf
-                          @method('DELETE')
-                          <button type="submit" class="btn btn-sm btn-danger">
-                              <i class="fa-solid fa-trash-can"></i>
-                          </button>
-                      </form>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          @endforeach
-        </table>
-      </div>
-    </div>
-</div>
+  
 @endsection
