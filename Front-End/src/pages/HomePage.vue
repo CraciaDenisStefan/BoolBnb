2953399ef1f45f2aaf9a2e52981106830c8f3f23
CraciDenisStefan <crdenis13@gmail.com>
2023-09-20 14:40:03 +0200

    Card vue ok

--- Front-End/src/pages/HomePage.vue
+++ Front-End/src/pages/HomePage.vue
@@ -9,7 +9,8 @@ export default {
     data() {
         return {
             apartments: [],
-            store
+            store,
+            maxCaracters: 30,
         }
     },
     created() {
@@ -28,19 +29,35 @@ export default {
                     }
                 });
             },
+        truncateText(text) {
+                if (text.length > this.maxCaracters) {
+                    return text.substr(0, this.maxCaracters) + '...';
+                }
+                return text
+                }
     },
+
 }
 </script>
-<template>
-    <AppLoading v-if="this.store.loading"/>
-    <div v-else>
-        <div class="container">
-            <div class="row">
-                <div v-for="apartment in apartments" :key="apartment.id">
-                    <div class="col-12 col-md-6">
-                        <div class="card">
-                            {{ apartment.title }}
-                            <img class="card-img-top my-img" :src="`${this.store.baseUrl}/storage/${apartment.img}`" :alt="'Immagine non disponibile'">
+<template>    
+    <div class="container">
+        <div class="row">
+            <div v-for="apartment in apartments" :key="apartment.id" class="col-12 col-lg-6 mb-4"> 
+                <div class="card border m-2" >
+                    <div class="row g-0">
+                        <div  class="col-md-4">
+                            <div v-if="apartment.img" class="card-img my_card shadow-lg"
+                                :style="{ backgroundImage: `url('${this.store.baseUrl}/storage/${apartment.img}')`,backgroundSize: 'cover', backgroundPosition: 'center', height: '150px'  }">
+                            </div>
+                            <div v-else class="card-img my_card"
+                                style="background-image: url('https://vestnorden.com/wp-content/uploads/2018/03/house-placeholder.png'); background-size: cover; background-position: center; height: 150px;">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title" v-if="apartment.title">{{ apartment.title }}</h5>
+                                <p class="card-text truncate-text" v-if="apartment.description">{{ truncateText(apartment.description) }}</p> 
+                            </div>            
                         </div>
                     </div>
                 </div>
