3741f7021f17aac3c39ac005848d1e39d1f5bbec
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-09-20 10:16:47 +0200

    First Comment Vite

---
+++ Front-End/src/App.vue
@@ -0,0 +1,18 @@
+<script >
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components:{
+      AppHeader
+    },
+}
+</script>
+
+<template>
+  <div>
+    <AppHeader />
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
